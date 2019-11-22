5fd215920cdd1a610eefd97604ae5d753e7627b0
17034460221 <17034460221@hadoop.com>
2019-11-22 20:41:44 +0800

    实验6

---
+++ hadoopch06/src/test/java/com/hadoop/ch06/v17034460221/AppTest.java
@@ -0,0 +1,20 @@
+package com.hadoop.ch06.v17034460221;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
